aa34080889eb3e287ee42c356bb39d52a5511981
Nikita Podvirnyi <krypt0nn@vk.com>
2025-08-22 18:12:34 +0200

    feat(storage): update storage trait

--- src/storage/file_storage.rs
+++ src/storage/file_storage.rs
@@ -11,12 +11,8 @@ impl FileStorage {
     pub fn open(path: impl Into<PathBuf>) -> std::io::Result<Self> {
         let path: PathBuf = path.into();
 
-        if !path.join("transactions").is_dir() {
-            std::fs::create_dir_all(path.join("transactions"))?;
-        }
-
-        if !path.join("blocks").is_dir() {
-            std::fs::create_dir_all(path.join("blocks"))?;
+        if !path.is_dir() {
+            std::fs::create_dir_all(&path)?;
         }
 
         if !path.join("index").is_file() {
@@ -26,79 +22,85 @@ impl FileStorage {
         Ok(Self(path))
     }
 
-    fn get_transaction_path(&self, hash: &Hash) -> PathBuf {
-        self.0.join("transactions")
-            .join(format!("{:0x}", hash.0[0]))
-            .join(format!("{:0x}", hash.0[1]))
-            .join(hash.to_base64())
-    }
-
     fn get_block_path(&self, hash: &Hash) -> PathBuf {
-        self.0.join("blocks")
+        self.0
             .join(format!("{:0x}", hash.0[0]))
             .join(format!("{:0x}", hash.0[1]))
             .join(hash.to_base64())
     }
 
-    fn read_block_from_index(&self, mut index: u64) -> std::io::Result<Option<Hash>> {
-        let mut db = File::open(self.0.join("index"))?;
+    fn index_size(&self) -> std::io::Result<u64> {
+        Ok(self.0.join("index").metadata()?.len() / 32)
+    }
 
-        index *= 32;
-
-        if index > db.metadata()?.len() {
+    fn index_read_block_hash(
+        &self,
+        index: u64
+    ) -> std::io::Result<Option<Hash>> {
+        if self.index_size()? <= index {
             return Ok(None);
         }
 
-        let mut hash = [0; 32];
-
-        db.seek(SeekFrom::Start(index))?;
-        db.read_exact(&mut hash)?;
-
-        Ok(Some(Hash::from(hash)))
-    }
-
-    fn read_last_block_hash(&self) -> std::io::Result<Option<Hash>> {
-        let mut db = File::open(self.0.join("index"))?;
-
-        if db.metadata()?.len() < 32 {
-            return Ok(None);
-        }
-
-        let mut hash = [0; 32];
-
-        db.seek(SeekFrom::End(-32))?;
-        db.read_exact(&mut hash)?;
-
-        Ok(Some(Hash::from(hash)))
-    }
-
-    fn append_block_to_index(&self, hash: &Hash) -> std::io::Result<()> {
-        let mut db = File::options()
-            .append(true)
+        let mut file = File::options()
+            .create(true)
+            .truncate(false)
+            .read(true)
             .open(self.0.join("index"))?;
 
-        db.write_all(&hash.0)?;
-        db.flush()?;
+        let mut hash = [0; 32];
+
+        file.seek(SeekFrom::Start(index * 32))?;
+        file.read_exact(&mut hash)?;
+
+        Ok(Some(Hash::from(hash)))
+    }
+
+    fn index_write_block_hash(
+        &self,
+        index: u64,
+        hash: &Hash
+    ) -> std::io::Result<()> {
+        let mut file = File::options()
+            .create(true)
+            .truncate(false)
+            .write(true)
+            .open(self.0.join("index"))?;
+
+        file.set_len(index * 32)?;
+        file.seek(SeekFrom::Start(index * 32))?;
+        file.write_all(&hash.0)?;
+        file.flush()?;
 
         Ok(())
+    }
+
+    fn index_find_block_hash(
+        &self,
+        hash: &Hash
+    ) -> std::io::Result<Option<u64>> {
+        let mut file = File::options()
+            .create(true)
+            .truncate(false)
+            .read(true)
+            .open(self.0.join("index"))?;
+
+        let mut current_hash = [0; 32];
+        let mut i = 0;
+
+        while file.read_exact(&mut current_hash).is_ok() {
+            if current_hash == hash.0 {
+                return Ok(Some(i));
+            }
+
+            i += 1;
+        }
+
+        Ok(None)
     }
 }
 
 impl Storage for FileStorage {
     type Error = std::io::Error;
-
-    fn read_transaction(&self, hash: &Hash) -> Result<Option<Transaction>, Self::Error> {
-        let path = self.get_transaction_path(hash);
-
-        if !path.is_file() {
-            return Ok(None);
-        }
-
-        let transaction = std::fs::read(path)?;
-        let transaction = Transaction::from_bytes(transaction)?;
-
-        Ok(Some(transaction))
-    }
 
     fn read_block(&self, hash: &Hash) -> Result<Option<Block>, Self::Error> {
         let path = self.get_block_path(hash);
@@ -113,59 +115,79 @@ impl Storage for FileStorage {
         Ok(Some(block))
     }
 
-    fn read_last_block(&self) -> Result<Option<Block>, Self::Error> {
-        match self.read_last_block_hash()? {
+    fn read_next_block(
+        &self,
+        hash: &Hash
+    ) -> Result<Option<Block>, Self::Error> {
+        match self.index_find_block_hash(hash)? {
+            Some(index) => {
+                match self.index_read_block_hash(index + 1)? {
+                    Some(hash) => self.read_block(&hash),
+                    None => Ok(None)
+                }
+            }
+
+            None => Ok(None)
+        }
+    }
+
+    fn read_first_block(&self) -> Result<Option<Block>, Self::Error> {
+        match self.index_read_block_hash(0)? {
             Some(hash) => self.read_block(&hash),
             None => Ok(None)
         }
     }
 
-    fn index_block(&self, index: u64) -> Result<Option<Block>, Self::Error> {
-        match self.read_block_from_index(index)? {
+    fn read_last_block(&self) -> Result<Option<Block>, Self::Error> {
+        let size = self.index_size()?;
+
+        if size == 0 {
+            return Ok(None);
+        }
+
+        match self.index_read_block_hash(size - 1)? {
             Some(hash) => self.read_block(&hash),
             None => Ok(None)
         }
     }
 
     fn write_block(&self, block: Block) -> Result<(), Self::Error> {
+        let Some(index) = self.index_find_block_hash(block.previous())? else {
+            return Err(std::io::Error::other("can't write block if parent block is not written yet"));
+        };
+
         let hash = block.hash()?;
         let path = self.get_block_path(&hash);
 
-        // TODO: obviously very inefficient, this is just a hack
+        if let Some(parent) = path.parent() && !parent.is_dir() {
+            std::fs::create_dir_all(parent)?;
+        }
 
         std::fs::write(path, block.to_bytes()?)?;
 
-        if let BlockContent::Transactions(transactions) = block.content() {
-            for transaction in transactions {
-                let path = self.get_transaction_path(&transaction.hash());
-
-                std::fs::write(path, transaction.to_bytes()?)?;
-            }
-        }
-
-        self.append_block_to_index(&hash)?;
+        self.index_write_block_hash(index + 1, &hash)?;
 
         Ok(())
-    }
-
-    fn get_current_validators(&self) -> Result<Vec<PublicKey>, Self::Error> {
-        match self.read_last_block_hash()? {
-            Some(hash) => self.get_validators_for_block(&hash),
-            None => Ok(vec![])
-        }
     }
 
     fn get_validators_for_block(&self, hash: &Hash) -> Result<Vec<PublicKey>, Self::Error> {
         let mut block = self.read_block(hash)?;
 
         while let Some(value) = block {
+            block = self.read_block(value.previous())?;
+
             if let BlockContent::Validators(validators) = value.content() {
                 return Ok(validators.to_vec());
             }
-
-            block = self.read_block(value.previous())?;
         }
 
         Ok(vec![])
+    }
+
+    fn get_current_validators(&self) -> Result<Vec<PublicKey>, Self::Error> {
+        match self.read_last_block()? {
+            Some(block) => self.get_validators_for_block(&block.hash()?),
+            None => Ok(vec![])
+        }
     }
 }
--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,5 +1,4 @@
 use crate::crypto::*;
-use crate::transaction::*;
 use crate::block::*;
 
 pub mod file_storage;
@@ -7,19 +6,28 @@ pub mod file_storage;
 pub trait Storage {
     type Error;
 
-    /// Read transaction from its hash.
-    fn read_transaction(&self, hash: &Hash) -> Result<Option<Transaction>, Self::Error>;
-
-    /// Read block from its hash.
+    /// Read block from its hash. Return `Ok(None)` if there's no such block.
     fn read_block(&self, hash: &Hash) -> Result<Option<Block>, Self::Error>;
 
-    /// Read last block.
+    /// Read block next to the one with provided hash. Return `Ok(None)` if
+    /// there's no block next to the requested one.
+    ///
+    /// ```text,no_run
+    /// [curr_block] <--- [next_block]
+    ///                   ^^^^^^^^^^^^ returned value
+    /// ```
+    fn read_next_block(
+        &self,
+        hash: &Hash
+    ) -> Result<Option<Block>, Self::Error>;
+
+    /// Read the first block. Return `Ok(None)` if blockchain is empty.
+    fn read_first_block(&self) -> Result<Option<Block>, Self::Error>;
+
+    /// Read the last block. Return `Ok(None)` if blockchain is empty.
     fn read_last_block(&self) -> Result<Option<Block>, Self::Error>;
 
-    /// Read block from its index in the chain.
-    fn index_block(&self, index: u64) -> Result<Option<Block>, Self::Error>;
-
-    /// Write block to the chain.
+    /// Write block to the blockchain.
     ///
     /// This method must automatically prune excess blocks if this one modifies
     /// the history, and write all the transactions from the block's content if
@@ -28,15 +36,20 @@ pub trait Storage {
     /// verifications, even if the block is not valid.
     fn write_block(&self, block: Block) -> Result<(), Self::Error>;
 
+    /// Get list of blockchain validators at the point in time when the block
+    /// with provided hash didn't exist yet. This is needed because validators
+    /// can change over time and we want to know which validators existed at any
+    /// point in time.
+    ///
+    /// The "didn't exist" rule means that if the block with provided hash
+    /// changes validators list then this method should return *previous
+    /// validators* list - so validators who can approve creation of the block
+    /// with provided hash.
+    fn get_validators_for_block(
+        &self,
+        hash: &Hash
+    ) -> Result<Vec<PublicKey>, Self::Error>;
+
     /// Get list of current blockchain validators.
     fn get_current_validators(&self) -> Result<Vec<PublicKey>, Self::Error>;
-
-    /// Get list of blockchain validators at the point in time when the block
-    /// with provided hash existed. This is needed because validators can change
-    /// over time and we want to know which validators existed at any point in
-    /// time.
-    ///
-    /// Result of this method must be equal to the `get_current_validators` for
-    /// the last block in blockchain.
-    fn get_validators_for_block(&self, hash: &Hash) -> Result<Vec<PublicKey>, Self::Error>;
 }
