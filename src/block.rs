0dad8f8c13e183afdf34626a49b6244e7e1b0a1a
Nikita Podvirnyi <krypt0nn@vk.com>
2025-08-22 10:34:22 +0200

    init commit

---
+++ src/block.rs
@@ -0,0 +1,245 @@
+use std::io::{Read, Cursor};
+
+use time::UtcDateTime;
+use varint_rs::{VarintReader, VarintWriter};
+
+use crate::crypto::*;
+use crate::transaction::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Block {
+    previous: Hash,
+    timestamp: UtcDateTime,
+    sign: Signature,
+    content: BlockContent
+}
+
+impl Block {
+    /// Create new block from provided previous block's hash and content using
+    /// validator's secret key.
+    pub fn new(
+        validator: &SecretKey,
+        previous: impl Into<Hash>,
+        content: impl Into<BlockContent>
+    ) -> std::io::Result<Self> {
+        let previous: Hash = previous.into();
+        let content: BlockContent = content.into();
+
+        let timestamp = UtcDateTime::now();
+
+        let mut hasher = blake3::Hasher::new();
+
+        hasher.update(&previous.0);
+        hasher.update(&timestamp.unix_timestamp().to_be_bytes());
+        hasher.update(&content.to_bytes()?);
+
+        let hash = Hash::from(hasher.finalize());
+
+        let sign = Signature::create(validator, hash)
+            .map_err(std::io::Error::other)?;
+
+        Ok(Self {
+            previous,
+            timestamp,
+            sign,
+            content
+        })
+    }
+
+    #[inline(always)]
+    pub fn previous(&self) -> Hash {
+        self.previous
+    }
+
+    #[inline(always)]
+    pub fn timestamp(&self) -> UtcDateTime {
+        self.timestamp
+    }
+
+    #[inline(always)]
+    pub fn sign(&self) -> &Signature {
+        &self.sign
+    }
+
+    #[inline(always)]
+    pub fn content(&self) -> &BlockContent {
+        &self.content
+    }
+
+    /// Calculate hash of the current block.
+    pub fn hash(&self) -> std::io::Result<Hash> {
+        let mut hasher = blake3::Hasher::new();
+
+        hasher.update(&self.previous.0);
+        hasher.update(&self.timestamp.unix_timestamp().to_be_bytes());
+        hasher.update(&self.content.to_bytes()?);
+
+        Ok(Hash::from(hasher.finalize()))
+    }
+
+    /// Encode block into bytes representation.
+    pub fn to_bytes(&self) -> std::io::Result<Box<[u8]>> {
+        let content = self.content.to_bytes()?;
+        let timestamp = self.timestamp.unix_timestamp();
+
+        let mut block = Vec::new();
+
+        block.push(0);                      // Format version
+        block.extend(self.previous.0);      // Previous block's hash
+        block.write_i64_varint(timestamp)?; // Creation timestamp
+        block.extend(self.sign.to_bytes()); // Sign
+        block.extend(content);              // Content
+
+        Ok(block.into_boxed_slice())
+    }
+
+    /// Decode block from bytes representation.
+    pub fn from_bytes(block: impl AsRef<[u8]>) -> std::io::Result<Self> {
+        let block = block.as_ref();
+
+        if block.is_empty() {
+            return Err(std::io::Error::other("invalid block length"));
+        }
+
+        if block[0] != 0 {
+            return Err(std::io::Error::other("unknown block format"));
+        }
+
+        let mut previous = [0; 32];
+
+        previous.copy_from_slice(&block[1..33]);
+
+        let mut block = Cursor::new(block[33..].to_vec());
+
+        let timestamp = block.read_i64_varint()?;
+
+        let timestamp = UtcDateTime::from_unix_timestamp(timestamp)
+            .map_err(|_| std::io::Error::other("invalid timestamp format"))?;
+
+        let mut sign = [0; 64];
+
+        block.read_exact(&mut sign)?;
+
+        let sign = Signature::from_bytes(sign)
+            .ok_or_else(|| std::io::Error::other("invalid signature format"))?;
+
+        let mut content = Vec::new();
+
+        block.read_to_end(&mut content)?;
+
+        Ok(Self {
+            previous: Hash::from(previous),
+            timestamp,
+            sign,
+            content: BlockContent::from_bytes(content)?
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum BlockContent {
+    /// List of approved validators' public keys.
+    Validators(Box<[PublicKey]>),
+
+    /// List of approved transactions.
+    Transactions(Box<[Transaction]>)
+}
+
+impl BlockContent {
+    /// Create new validators block.
+    pub fn validators<T: Into<PublicKey>>(
+        public_keys: impl IntoIterator<Item = T>
+    ) -> Self {
+        Self::Validators(public_keys.into_iter().map(T::into).collect())
+    }
+
+    /// Create new transactions block.
+    pub fn transactions<T: Into<Transaction>>(
+        transactions: impl IntoIterator<Item = T>
+    ) -> Self {
+        Self::Transactions(transactions.into_iter().map(T::into).collect())
+    }
+
+    /// Encode block's content into bytes representation.
+    pub fn to_bytes(&self) -> std::io::Result<Box<[u8]>> {
+        let mut content = Vec::new();
+
+        match self {
+            Self::Validators(validators) => {
+                content.push(0); // validators v1 format
+
+                for validator in validators {
+                    content.extend(validator.to_bytes());
+                }
+            }
+
+            Self::Transactions(transactions) => {
+                content.push(1); // transactions v1 format
+
+                for transaction in transactions {
+                    let transaction = transaction.to_bytes()?;
+
+                    content.write_usize_varint(transaction.len())?;
+                    content.extend(transaction);
+                }
+            }
+        }
+
+        Ok(content.into_boxed_slice())
+    }
+
+    /// Decode block's content from bytes representation.
+    pub fn from_bytes(content: impl AsRef<[u8]>) -> std::io::Result<Self> {
+        let content = content.as_ref();
+        let n = content.len();
+
+        if n == 0 {
+            return Err(std::io::Error::other("block's content can't be empty"));
+        }
+
+        match content[0] {
+            // validators v1 format
+            0 => {
+                if (n - 1) % 33 != 0 {
+                    return Err(std::io::Error::other("invalid validators block format"));
+                }
+
+                let mut validators = Vec::with_capacity((n - 1) / 33);
+                let mut i = 1;
+
+                while i < n {
+                    let validator = PublicKey::from_bytes(&content[i..i + 33])
+                        .ok_or_else(|| std::io::Error::other("invalid validator's public key format"))?;
+
+                    validators.push(validator);
+
+                    i += 33;
+                }
+
+                Ok(Self::Validators(validators.into_boxed_slice()))
+            }
+
+            // transactions v1 format
+            1 => {
+                let mut content = Cursor::new(content[1..].to_vec());
+                let mut transactions = Vec::new();
+
+                while content.position() < n as u64 {
+                    let len = content.read_usize_varint()?;
+
+                    let mut transaction = vec![0; len];
+
+                    content.read_exact(&mut transaction[..len])?;
+
+                    let transaction = Transaction::from_bytes(transaction)?;
+
+                    transactions.push(transaction);
+                }
+
+                Ok(Self::Transactions(transactions.into_boxed_slice()))
+            }
+
+            _ => Err(std::io::Error::other("unknown block content format"))
+        }
+    }
+}
---
+++ src/transaction.rs
@@ -0,0 +1,187 @@
+use std::io::Cursor;
+
+use serde_json::{json, Value as Json};
+
+use crate::crypto::*;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Transaction {
+    sign: Signature,
+    data: Box<[u8]>
+}
+
+impl Transaction {
+    /// Create new transaction from the given data using provided secret key.
+    pub fn create(
+        secret_key: &SecretKey,
+        data: impl Into<Box<[u8]>>
+    ) -> Result<Self, k256::ecdsa::Error> {
+        let data: Box<[u8]> = data.into();
+        let hash = Hash::from_slice(&data);
+
+        let sign = Signature::create(secret_key, hash)?;
+
+        Ok(Self {
+            sign,
+            data
+        })
+    }
+
+    /// Calculate hash of transaction.
+    #[inline]
+    pub fn hash(&self) -> Hash {
+        Hash::from_slice(&self.data)
+    }
+
+    /// Get transaction's data.
+    #[inline(always)]
+    pub fn data(&self) -> &[u8] {
+        &self.data
+    }
+
+    /// Get transaction's signature.
+    #[inline(always)]
+    pub fn sign(&self) -> &Signature {
+        &self.sign
+    }
+
+    /// Derive transaction's author and verify its content.
+    pub fn verify(&self) -> Result<(bool, PublicKey), k256::ecdsa::Error> {
+        self.sign.verify(self.hash())
+    }
+
+    /// Get bytes representation of the current transaction.
+    pub fn to_bytes(&self) -> std::io::Result<Box<[u8]>> {
+        let sign = self.sign.to_bytes();
+
+        let mut bytes = Vec::with_capacity(66 + self.data.len());
+
+        bytes.push(0);                       // Format version
+        bytes.extend(sign);                  // Fixed-size sign
+        bytes.extend_from_slice(&self.data); // Transaction data
+
+        let bytes = zstd::encode_all(Cursor::new(bytes), 0)?;
+
+        Ok(bytes.into_boxed_slice())
+    }
+
+    /// Decode transaction from bytes representation.
+    pub fn from_bytes(bytes: impl AsRef<[u8]>) -> std::io::Result<Self> {
+        let bytes = zstd::decode_all(bytes.as_ref())?;
+
+        if bytes.len() < 66 {
+            return Err(std::io::Error::other("invalid transaction bytes len"));
+        }
+
+        if bytes[0] != 0 {
+            return Err(std::io::Error::other("unknown transaction format"));
+        }
+
+        let mut sign = [0; 65];
+
+        sign.copy_from_slice(&bytes[1..66]);
+
+        Ok(Self {
+            sign: Signature::from_bytes(sign)
+                .ok_or_else(|| std::io::Error::other("invalid signature format"))?,
+
+            data: bytes[66..].to_vec().into_boxed_slice()
+        })
+    }
+
+    /// Get standard JSON representation of the transaction.
+    pub fn to_json(&self) -> std::io::Result<Json> {
+        let data = zstd::encode_all(Cursor::new(&self.data), 0)?;
+
+        Ok(json!({
+            "format": 0,
+            "sign": self.sign.to_base64(),
+            "data": base64_encode(data)
+        }))
+    }
+
+    /// Decode standard JSON representation of the transaction.
+    pub fn from_json(transaction: &Json) -> std::io::Result<Self> {
+        if transaction.get("format").and_then(Json::as_u64) != Some(0) {
+            return Err(std::io::Error::other("unknown transaction format"));
+        }
+
+        let Some(sign) = transaction.get("sign").and_then(Json::as_str) else {
+            return Err(std::io::Error::other("missing transaction sign"));
+        };
+
+        let sign = Signature::from_base64(sign)
+            .ok_or_else(|| std::io::Error::other("invalid signature format"))?;
+
+        let Some(data) = transaction.get("data").and_then(Json::as_str) else {
+            return Err(std::io::Error::other("missing transaction data"));
+        };
+
+        let data = base64_decode(data)
+            .map_err(std::io::Error::other)?;
+
+        let data = zstd::decode_all(data.as_slice())?;
+
+        Ok(Self {
+            sign,
+            data: data.into_boxed_slice()
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn get_transaction() -> Result<(SecretKey, Transaction), Box<dyn std::error::Error>> {
+        use rand_chacha::rand_core::SeedableRng;
+
+        let mut rand = rand_chacha::ChaCha20Rng::seed_from_u64(123);
+
+        let secret_key = SecretKey::random(&mut rand);
+
+        let transaction = Transaction::create(
+            &secret_key,
+            b"hello, world!".to_vec()
+        )?;
+
+        Ok((secret_key, transaction))
+    }
+
+    #[test]
+    fn validate() -> Result<(), Box<dyn std::error::Error>> {
+        let (secret_key, transaction) = get_transaction()?;
+
+        let (valid, author) = transaction.verify()?;
+
+        assert!(valid);
+
+        assert_eq!(author, secret_key.public_key());
+        assert_eq!(transaction.data(), b"hello, world!");
+        assert_eq!(transaction.hash().to_base64(), "W5KgqE-8UKWMdPRxe8DV9AMoKuTNfXo4QxHtPEGKFdg=");
+
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_bytes() -> Result<(), Box<dyn std::error::Error>> {
+        let (_, transaction) = get_transaction()?;
+
+        let deserialized = Transaction::from_bytes(transaction.to_bytes()?)?;
+
+        assert_eq!(transaction, deserialized);
+
+        Ok(())
+    }
+
+    #[test]
+    fn serialize_json() -> Result<(), Box<dyn std::error::Error>> {
+        let (_, transaction) = get_transaction()?;
+
+        let deserialized = Transaction::from_json(&transaction.to_json()?)?;
+
+        assert_eq!(transaction, deserialized);
+
+        Ok(())
+    }
+}
