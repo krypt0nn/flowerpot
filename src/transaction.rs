490069c4824ffb6fbd85109712d687159a9f7ef3
Nikita Podvirnyi <krypt0nn@vk.com>
2025-08-30 19:24:07 +0200

    chore: add license comment to every source file

--- src/transaction.rs
+++ src/transaction.rs
@@ -1,10 +1,28 @@
+// SPDX-License-Identifier: GPL-3.0-only
+//
+// libflowerpot
+// Copyright (C) 2025  Nikita Podvirnyi <krypt0nn@vk.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::io::Cursor;
 
 use serde_json::{json, Value as Json};
 
 use crate::crypto::*;
 
-pub const TRANSACTION_COMPRESSION_LEVEL: i32 = 0;
+const TRANSACTION_COMPRESSION_LEVEL: i32 = 0;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Transaction {
